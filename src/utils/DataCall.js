cc76204f0d36197a0ebb422fefa010288df9c34b
visopsys <billypham08@gmail.com>
2019-04-21 21:41:54 -0700

    Use fixed data

--- src/utils/DataCall.js
+++ src/utils/DataCall.js
@@ -1,11 +1,30 @@
+let data = [
+  'https://i.imgur.com/oxss7Kr.jpg',
+  'https://i.imgur.com/M70OEe2.jpg',
+  'https://i.imgur.com/kNQ3Lkf.jpg',
+  'https://i.imgur.com/BrhpQnV.jpg',
+  'https://i.imgur.com/ScJRZpQ.jpg',
+  'https://i.imgur.com/QtDkUZR.jpg',
+  'https://i.imgur.com/qeqpR8f.jpg',
+  'https://i.imgur.com/R1dFrxj.jpg',
+  'https://i.imgur.com/puTjEVW.jpg',
+  'https://i.imgur.com/UPd9gqk.jpg',
+  'https://i.imgur.com/p0pTlep.jpg',
+  'https://i.imgur.com/RQAEeB2.jpg',
+  'https://i.imgur.com/jiq2hh7.jpg',
+  'https://i.imgur.com/LEwaaPe.jpg',
+];
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export class DataCall {
+
   // Just simulating incremental loading, don't infer anything from here
-  static async get() {
-    const firstResponse = await fetch(
-      "http://5cb9f093f841d2001455e8fe.mockapi.io/sports"
-    );
-    const firstResponseJson = await firstResponse.json();
-    const fullData = firstResponseJson.message
-    return fullData;
+  static async get(start, count) {
+    await sleep(2000);
+
+    return data;
   }
 }
